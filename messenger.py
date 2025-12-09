0690b4376687e611e32c35103b39bb5655911c0e
Romain Tartivel <romain.tartivel@etu.minesparis.psl.eu>
2025-12-09 16:36:30 +0100

    ajout des messages et on passe aux classes

--- messenger.py
+++ messenger.py
@@ -8,6 +8,8 @@ def save_server():
     with open('server.json', 'w') as file:
         json.dump(server, file, indent=4)
 
+
+
 def afficher_utilisateur():
     for user in server['users']:
         idnom = str(user['id']) + '. ' + user['name']
@@ -18,11 +20,45 @@ def afficher_groupes():
     for chan in server['channels']:
         print(chan['id'],chan['name'])
 
+
+
+def send_messages(choicechannel : int):
+    user_id = int(input('Enter your user id: '))
+    content = input('Write a message: ')
+    
+    new_message = {
+        'id': len(server['messages']) + 1,
+        'channel': choicechannel,
+        'sender_id': user_id,
+        'content': content,
+        'reception_date': datetime.now().isoformat()
+
+ }
+   
+    server['messages'].append(new_message)
+    save_server()
+    for mess in server['messages'] :
+            if mess['channel'] == choicechannel :
+                print(mess['reception_date'][:16].replace('T', ' '), mess['content'])
+    choicemessage = input('Envoyer un autre message ? (oui/non) : ')
+    if choicemessage == 'oui' :
+        user_id = int(input('Enter your user id: '))
+        send_messages(choicechannel)
+    else : 
+        menu()
+
+
+
+
 def afficher_messages():
     choicechannel = int(input('Select a channel id : '))
     for mess in server['messages'] :
         if mess['channel'] == choicechannel :
-            print(mess['content'])
+            print(mess['reception_date'][:16].replace('T', ' '), mess['content'])
+    send_messages(choicechannel)
+
+
+
 
 def ajouter_utilisateur():
     identifiant = int(input('identifiant: '))
@@ -32,7 +68,7 @@ def ajouter_utilisateur():
     save_server()
 
 def ajouter_groupe():
-    identifiant = int(input('identifiant du groupe: '))
+    identifiant = max(channel['id'] for channel in server['channels']) + 1
     nom = str(input('nom du groupe: '))
     membres = []
     ajout=input('Encore un nouveau membre ? (oui/non) : ')
@@ -43,6 +79,7 @@ def ajouter_groupe():
     nouveaugroupe={'id': identifiant, 'name': nom, 'member_ids': membres}
     server['channels'].append(nouveaugroupe)
     save_server()
+
 
 
 
